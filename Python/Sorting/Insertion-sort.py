8e641236b2e37b23132a41af247aa0d9cc3cc785
Vivek Vijayan <vivekvijayan3@icloud.com>
2021-02-16 13:24:11 +0530

    Updated the functional call in C++ and Python files

--- Python/Sorting/Insertion-sort.py
+++ Python/Sorting/Insertion-sort.py
@@ -1,27 +1,32 @@
 
 # This code is contributed by Vivek Vijayan
 # http://github.com/vivek-vijayan
-# Sorting Head - start with 1
-SortingHead = 1   
-
-# Array list
-array = []
-
-while str(input("Add new value? (y/n) ")) == "y":
-    array.append(input("List input  :"))
-
-
+  
 # Main Algorithm - Insertion Algorithm
-while SortingHead != len(array):
-    if(array[SortingHead] < array[SortingHead - 1]):
-        # Swap the element if a[i] < a[i-1]
-        array[SortingHead], array[SortingHead - 1] = array[SortingHead - 1], array[SortingHead]
-        
-        # Bring the header to before element as it is swapped. ignore if it swapped the first element as index is 0 
-        if SortingHead != 1:
-            SortingHead -= 1
-    else:
-        SortingHead += 1
+def InsertionSort(array):
+    # Sorting Head - start with 1
+    SortingHead = 1 
+    while SortingHead != len(array):
+        if(array[SortingHead] < array[SortingHead - 1]):
+            # Swap the element if a[i] < a[i-1]
+            array[SortingHead], array[SortingHead - 1] = array[SortingHead - 1], array[SortingHead]
+            
+            # Bring the header to before element as it is swapped. ignore if it swapped the first element as index is 0 
+            if SortingHead != 1:
+                SortingHead -= 1
+        else:
+            SortingHead += 1
+    return array
 
-# Printing the sorted array
-print(array)
+
+if __name__== "__main__":
+    # Array list
+    array = []
+    while str(input("Add new value? (y/n) ")) == "y":
+        array.append(input("List input  :"))
+    # Printing the sorted array
+    print(InsertionSort(array))
+
+
+
+
